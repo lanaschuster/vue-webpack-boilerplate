829150206929b79324e8f5dedbe5143ac7bdd6bf
Lana Schuster <lanaschuster1@gmail.com>
2022-09-18 11:57:20 -0300

    test: ensure AppButton emits click event once when clicked

--- src/components/AppButton/AppButton.spec.js
+++ src/components/AppButton/AppButton.spec.js
@@ -1,5 +1,11 @@
-describe('sanity test', () => {
-  it('testing configuration', () => {
-    expect(1).toBe(1)
-  })
-})
+import { shallowMount } from '@vue/test-utils';
+import AppButton from './AppButton';
+
+describe('Testing AppButton component', () => {
+  it('emits a click event when clicked', async () => {
+    const wrapper = shallowMount(AppButton);
+    await wrapper.trigger('click');
+    expect(wrapper.emitted()['click']).toBeTruthy();
+    expect(wrapper.emitted()['click'].length).toBe(1);
+  });
+});
