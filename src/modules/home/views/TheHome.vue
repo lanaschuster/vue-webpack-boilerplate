3428ca9b0118bcc8b79ed4a0f657860b4ae1bfeb
Lana Schuster <lanaschuster1@gmail.com>
2022-10-18 21:11:36 -0300

    feat: add random poetry component

--- src/modules/home/views/TheHome.vue
+++ src/modules/home/views/TheHome.vue
@@ -3,27 +3,25 @@
     <h1 class="title">
       Vue 3 + Webpack 5 Boilerplate
     </h1>
-    <h2>{{ title }}</h2>
+    <RandomPoetry />
   </section>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { defineAsyncComponent } from 'vue';
+
+const RandomPoetry = defineAsyncComponent(() => import('../components/RandomPoetry/RandomPoetry.vue'));
 
 export default {
-  setup() {
-    const title = computed(() => 'Uma aplicação base para você começar o seu projeto mais rápido!');
-
-    return {
-      title
-    };
+  components: {
+    RandomPoetry
   }
 };
 </script>
 
 <style lang="scss" scoped>
 section {
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
