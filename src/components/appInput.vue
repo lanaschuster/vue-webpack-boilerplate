1e916ccb1200aba969f01f1d225930c237a4b071
Lana Schuster <lanaschuster1@gmail.com>
2023-05-04 18:54:59 -0300

    feat: add appInput component

---
+++ src/components/appInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+    v-model="model"
+    class="input" 
+  >
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+    required: true
+  }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const model = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+  }
+});
+</script>
