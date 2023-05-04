d003692fbe82f5e9acda3d29f1f88c47e9dc9434
Lana Schuster <lanaschuster1@gmail.com>
2023-05-04 18:40:30 -0300

    feat: add appModal component

---
+++ src/components/appModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <teleport to="body"> 
+    <div class="modal is-active">
+      <div class="modal-background" />
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <slot name="header">
+            <p class="modal-card-title">
+              {{ title }}
+            </p>
+            <button 
+              class="delete" 
+              aria-label="close"
+            />
+          </slot>
+        </header>
+        <section class="modal-card-body">
+          <slot />
+        </section>
+        <footer class="modal-card-foot">
+          <slot name="footer" />
+        </footer>
+      </div>
+    </div>
+  </teleport>
+</template>
